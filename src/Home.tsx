3dbfa835b0a19d4dd4a653dd12d03b367dd89d27
MrPewPewLaser <nevadagreif@gmail.com>
2021-05-29 01:13:45 +0200

    Building the basic site

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,38 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './Home.scss';
+import styles from './Home.module.scss';
 import { IHomeProps } from './Interfaces/IHomeProps';
 import { IHomeState } from './Interfaces/IHomeState';
+
+// Components
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import MenuIcon from '@material-ui/icons/Menu';
+import Info from '@material-ui/icons/Info';
+
+// Theme
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { themeOptions } from './Theme/Theme';
+
+const theme = createMuiTheme(themeOptions);
+
 
 export default class Home extends React.Component<IHomeProps, IHomeState> {
   constructor(props: IHomeProps) {
     super(props);
-
     this.state = {
 
     }
   }
 
   public render() {
-    return (
-      <div>
-        <AppBar>
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              {/* <MenuIcon /> */}
-            </IconButton>
-            <Typography variant="h6">
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+    return (      
+      <ThemeProvider theme={theme}>
+        <Box bgcolor={theme.palette.background.default} className={styles.bgImage} />
+        <Box bgcolor={theme.palette.background.default}>
+          <AppBar color={"inherit"} >
+            <Toolbar>
+              {/* <IconButton edge="start" color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton> */}
+              <Typography variant="h4" color={"textPrimary"} className={styles.title}>
+                MrPewPewLaser
+              </Typography>
+              <IconButton>
+                <Info />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <div>
+          
+        </div>
+      </ThemeProvider>
     );
   }
 }
---
+++ src/Theme/Theme.ts
@@ -0,0 +1,23 @@
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#ffc3a6',
+    },
+    secondary: {
+      main: '#a6ffc3',
+    },
+    background: {
+      default: '#131416',
+      paper: '#17181a',
+    },
+    error: {
+      main: '#f32514',
+    },
+  },
+  typography: {
+    fontFamily: "shabby-chic",
+  }
+};
